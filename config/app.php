581be34ea1a83572e001a3dd489b69c2624d7518
Lucas Gonzalez <lucasmgonzalez1992@gmail.com>
2016-11-28 09:52:14 -0200

    Adicionando Socialite

--- config/app.php
+++ config/app.php
@@ -167,7 +167,7 @@ return [
          * Package Service Providers...
          */
 
-        //
+        Laravel\Socialite\SocialiteServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -225,6 +225,10 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        //Package Aliases
+
+        'Socialite' => Laravel\Socialite\Facades\Socialite::class,
 
     ],
 
