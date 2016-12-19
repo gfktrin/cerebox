52d78eeeeacbc918f32d5d214e544b2b59fecd81
Lucas Gonzalez <lucasmgonzalez1992@gmail.com>
2016-12-19 13:05:52 -0200

    colocando url redirect no config/services

--- config/services.php
+++ config/services.php
@@ -38,7 +38,7 @@ return [
 	'facebook' => [
 		'client_id' => '1887887891430795',
 		'client_secret' => '37b4f82d950a71222f1dbb31fea73701',
-		'redirect' => 'auth/facebook/callback',
+		'redirect' => url('auth/facebook/callback'),
 	],
 
 ];
