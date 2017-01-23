9d79348017c7e68536d3cd11cc6afee5fca5b5fa
Luke Gonzalez <lucasmgonzalez1992@gmail.com>
2017-01-23 00:21:55 -0200

    Fix na url

--- resources/views/app/my-projects.blade.php
+++ resources/views/app/my-projects.blade.php
@@ -30,7 +30,7 @@
                                         @php($invoice = $project->invoices()->fromUser(Auth::user()->id)->get()->first())
                                         {{ $invoice->getStatus() }}
                                         @if($invoice->status == 0)
-                                            <a href="{{ $invoice->redirectUrl() }}" title="Efetuar pagamento">
+                                            <a href="{{ $invoice->paymentUrl() }}" title="Efetuar pagamento">
                                                 <i class="material-icons">payment</i>
                                             </a>
                                         @endif
