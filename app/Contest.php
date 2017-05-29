2483466d9627f0848df58bb1142e96ab76db69a6
Gabriel Trindade <gfktrin@yahoo.com.br>
2017-05-28 21:27:16 -0300

    correcao

--- app/Contest.php
+++ app/Contest.php
@@ -101,7 +101,7 @@ class Contest extends Model
     }
 
     public function bestProjects($limit = 3,$exclude = []){
-        $ranking = $this->projects()->withCount('votes')->whereNotIn('id',$exclude)->get()->sortBy('position')->take($limit);
+        $ranking = $this->projects()->get()->sortBy('position')->take($limit);
         return $ranking;
     }
 }
