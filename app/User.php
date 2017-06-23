05d43188fdfdae18335b044d11a12db8c30417cd
Gabriel Trindade <gfktrin@yahoo.com.br>
2017-06-23 20:50:14 -0300

    teste

--- app/User.php
+++ app/User.php
@@ -43,7 +43,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password','nickname','phone','city_id'
+        'name', 'email', 'password','nickname','phone','city_id','tickets'
     ];
 
     /**
